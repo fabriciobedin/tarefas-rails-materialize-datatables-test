f3d4a03bbe89a4f1e9e73b7dfbf5bb0db29f7114
Fabricio Bedin <fabricio-bedin@hotmail.com>
2017-12-09 17:59:13 -0200

    adicionando react no projeto

--- Gemfile
+++ Gemfile
@@ -53,6 +53,10 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem 'faker'
+gem 'rails_admin', '~>1.2'
+gem 'react-rails'
+
 gem 'jquery-rails'
 gem 'materialize-sass'
 gem 'material_icons'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'benchmark'
+puts Benchmark.measure {
+  500.times do
+    Tarefa.create(
+      titulo: Faker::Name.first_name,
+      descricao: Faker::Name.last_name,
+      status: Faker::Internet.email
+    )
+  end
+}
